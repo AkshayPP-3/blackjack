aa756b4eb248775bc84efe7b16ab0e86baacf6b1
Akshay <pps95026@gmail.com>
2025-03-31 14:27:35 +0530

    blackjack game

--- javascript.js
+++ javascript.js
@@ -1,15 +1,36 @@
-let counter= document.getElementById("counter")
-let pre=document.getElementById("pre")
-let count=0
-function increment(){
-    count+=1
-    counter.textContent=count
+let firstCard = 6
+let secondCard = 9
+let cards=[firstCard, secondCard]
+let sum = firstCard + secondCard
+let cardEl=document.getElementById("card-el")
+let sumEl=document.getElementById("sum-el")
+
+let msgEl=document.getElementById("msg")
+
+
+function startGame(){
+    cardEl.textContent = "Cards : "
+    for(let i=0; i<cards.length; i++){
+        cardEl.textContent+=cards[i]+ " "
+    }
+    sumEl.textContent= "Sum : "+ sum
+    if(sum<=20){
+        message= "Do you want to draw new card ? " 
+     }
+    else if(sum===21){
+       message= "You've got Blackjack"   
+    }
+    else {
+        message = "You're out of game!!!"
+    }
+    msg.innerText=message
 }
 
-function save(){
-    let countStr = count + "," 
-    pre.innerText += countStr
-    counter.textContent = 0
-    count = 0
-    
+function newCard(){
+    let card=7
+    sum+=card
+    cards.push(card)
+    startGame()
+    console.log(cards)
+
 }
